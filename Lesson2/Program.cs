0bfefd02c86846e5cb2789402b7fcedf48881be9
taliyevcode@gmail.com <taliyevcode@gmail.com>
2023-03-28 17:54:15 +0400

    Add project files.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Console and Parse
+            //Console.Title = "Account Manager";
+
+            //Console.WriteLine("Enter employee name: ");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Enter age: ");
+            //string ageString = Console.ReadLine();
+
+
+
+            //if(!int.TryParse(ageString, out int age))
+            //{
+            //    Console.BackgroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Not valid\n Plase Enter age correct!!!");
+            //    Console.ResetColor();
+            //    Console.WriteLine("Enter age: ");
+            //    ageString = Console.ReadLine();
+            //}
+
+
+            //Console.WriteLine("Education Degree: ");
+            //string education = Console.ReadLine();
+
+            //Console.WriteLine("Department: ");
+            //string department = Console.ReadLine();
+
+            //Console.WriteLine("Employee weight: ");
+            //string weigthString = Console.ReadLine();
+            //float weigth = Convert.ToSingle(weigthString);
+
+            //Console.WriteLine("Salary: ");
+            //string salaryString = Console.ReadLine();
+            //decimal salary = Convert.ToDecimal(salaryString);
+
+
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine($"Employee name: {name}\n" +
+            //    $"Employee age: {age}\n" +
+            //    $"Employee Education: {education}\n" +
+            //    $"Employee Department: {department}\n" +
+            //    $"Employee weight {weigth}\n" +
+            //    $"Employee salary {salary}"
+            //    );
+
+            //Console.ResetColor();
+            #endregion
+
+            #region Loops
+            //while
+
+            //bool isNotArrived = true;
+            //int time = default;
+
+            //while (isNotArrived)
+            //{
+            //    Console.WriteLine("Wait for Tural");
+            //    time += 5;
+            //    if (time > 30)
+            //    {
+            //        isNotArrived = false;
+            //    }
+            //}
+
+
+            // for
+            //int count = default;
+            //int.TryParse(Console.ReadLine(), out int number);
+
+
+            //for (int i = 2; i < number; i ++)
+            //{ // begin
+
+            //    if (count >= 1)
+            //    {
+            //        Console.WriteLine("Eded Sade Deyil");
+            //        continue;
+            //    }
+            //    if (number % i == 0)
+            //    {
+
+            //        count++;
+            //    }
+
+            //}//end
+
+            #endregion
+
+            //Console.WriteLine("Enter User Name: ");
+            //string userName = Console.ReadLine();
+
+            //Console.WriteLine("Enter position: ");
+            //string position = Console.ReadLine();
+
+            //Console.WriteLine("Enter Your weight: ");
+            //string ageString = Console.ReadLine();
+            //double.TryParse(ageString, out double age);
+            //while (age < 18 || age >= 66)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Please input age correctly! You have to be 18 years old!");
+            //    Console.ResetColor();
+
+            //    Console.WriteLine("Enter Your Age: ");
+            //    ageString = Console.ReadLine();
+            //    double.TryParse(ageString, out age);
+            //}
+
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.WriteLine("You Registered Correctly!");
+            //Console.ResetColor();
+
+            
+
+            
+
+
+
+        }
+    }
+}
